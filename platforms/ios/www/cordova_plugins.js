4d5c82dd2f680cd5918cc88872ada0210a9789b7
Pavithra Upadhya <pavithraupadhya@Pavithras-MacBook-Pro.local>
2018-12-06 10:27:40 +0530

    Creating test plugin for new Cryptoswift version for XCode 10.1

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,10 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-add-swift-support": "1.7.2",
+  "cordova-plugin-whitelist": "1.3.3"
+};
+// BOTTOM OF METADATA
+});
